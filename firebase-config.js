a6b2c34fb28096a72a161b012f8331ddda742f82
vnamana <33346973+vnamana@users.noreply.github.com>
2026-03-06 14:04:47 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCTKMCTjrnaBGsrSBKolmIXDuiAd-rXnEQ",
+  authDomain: "si-calculator-49215.firebaseapp.com",
+  projectId: "si-calculator-49215",
+  storageBucket: "si-calculator-49215.firebasestorage.app",
+  messagingSenderId: "388606784094",
+  appId: "1:388606784094:web:871353344cd28848cc3acf",
+  measurementId: "G-FR4B4DBSTZ",
+};
